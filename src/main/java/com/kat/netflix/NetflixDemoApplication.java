dc009762d2c557fb714ff16c64dc59c3220a2cd7
kat-kalyyan <32221979+kat-kalyyan@users.noreply.github.com>
2022-05-15 20:46:33 +0530

    Initial Commit

---
+++ src/main/java/com/kat/netflix/NetflixDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kat.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kat/netflix/NetflixDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat.netflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
